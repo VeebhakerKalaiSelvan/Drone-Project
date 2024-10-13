92f8c434e81b4dc58cc58442cca84881540ab880
VeebhakerKalaiSelvan <veebhaker_22007032@utp.edu.my>
2024-10-13 23:46:56 +0800

    Add files via upload

---
+++ FlockAgentHW2(Drone).cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class FlockAgent : MonoBehaviour
+{
+    Flock agentFlock;
+    public Flock AgentFlock { get { return agentFlock; } }
+
+    Collider2D agentCollider;
+    public Collider2D AgentCollider { get { return agentCollider; } }
+
+    // New attributes for search and rescue
+    public float DistanceToVictim { get; set; }
+    public float BatteryLife { get; set; } = 100f; // default battery percentage
+    public int VictimsFound { get; set; } = 0;
+    public bool HasFoundVictim { get; set; } = false; // Flag to check if the drone found a victim
+
+    void Start()
+    {
+        agentCollider = GetComponent<Collider2D>();
+    }
+
+    public void Initialize(Flock flock)
+    {
+        agentFlock = flock;
+    }
+
+    public void Move(Vector2 velocity)
+    {
+        // Only move if the drone hasn't found a victim
+        if (!HasFoundVictim)
+        {
+            transform.up = velocity;
+            transform.position += (Vector3)velocity * Time.deltaTime;
+        }
+    }
+}
